0ae0cccb70c2c33841a4919d73ada6ec2fc8e871
BlockchainCrazy95 <happyworld0905@gmail.com>
2022-07-12 12:03:29 +0200

    add cert

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 // make bluebird default Promise
 const { port, env } = require("./config/vars");
+const fs = require('fs')
+const https = require('https')
 const logger = require("./config/logger");
 const app = require("./config/express");
 
+/**
+ * Create HTTPS server.
+ */
+
+const server = https.createServer({
+    key: fs.readFileSync('./certs/private.key'),
+    cert: fs.readFileSync('./certs/certificate.crt'),
+}, app)
+
 // listen to requests
-app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
+server.listen(port, () => logger.info(`server started on port ${port} (${env})`));
 
 /**
  * Exports express
  * @public
  */
-module.exports = app;
+module.exports = server;
