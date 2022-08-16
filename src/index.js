251d249e5a8eb3ca28f7429d8be0bd829f2c466e
CryptoSnowPrince <topdirector2017@gmail.com>
2022-08-17 00:52:54 +0200

    update backend

--- src/index.js
+++ src/index.js
@@ -5,37 +5,33 @@ const https = require('https')
 const logger = require("./config/logger");
 const app = require("./config/express");
 
-////////////////////////////////////////////
-
 /**
  * Create HTTPS server.
  */
 
-// const server = https.createServer({
-//     key: fs.readFileSync('/certs/private.key'),
-//     cert: fs.readFileSync('/certs/certificate.crt'),
-// }, app)
-
-// // listen to requests
-// server.listen(port, () => logger.info(`server started on port ${port} (${env})`));
-
-// /**
-//  * Exports express
-//  * @public
-//  */
-// module.exports = server;
-
-////////////////////////////////////////////
-
-/**
- * Create HTTP server.
- */
+ const server = https.createServer({
+    key: fs.readFileSync('/etc/letsencrypt/live/cryptosnowprince.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/cryptosnowprince.com/fullchain.pem'),
+}, app)
 
 // listen to requests
-app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
+server.listen(port, () => logger.info(`server started on port ${port} (${env})`));
 
 /**
  * Exports express
  * @public
  */
-module.exports = app;
+module.exports = server;
+
+/**
+ * Create HTTP server.
+ */
+
+// // listen to requests
+// app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
+
+// /**
+//  * Exports express
+//  * @public
+//  */
+// module.exports = app;
