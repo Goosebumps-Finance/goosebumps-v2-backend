8f67451fa2bc8048f0fe9e639e0faa5b452ca125
CryptoSnowPrince <topdirector2017@gmail.com>
2022-08-16 21:47:31 +0200

    update apikey and port ant swith http server

--- src/index.js
+++ src/index.js
@@ -5,20 +5,37 @@ const https = require('https')
 const logger = require("./config/logger");
 const app = require("./config/express");
 
+////////////////////////////////////////////
+
 /**
  * Create HTTPS server.
  */
 
-const server = https.createServer({
-    key: fs.readFileSync('/certs/private.key'),
-    cert: fs.readFileSync('/certs/certificate.crt'),
-}, app)
+// const server = https.createServer({
+//     key: fs.readFileSync('/certs/private.key'),
+//     cert: fs.readFileSync('/certs/certificate.crt'),
+// }, app)
+
+// // listen to requests
+// server.listen(port, () => logger.info(`server started on port ${port} (${env})`));
+
+// /**
+//  * Exports express
+//  * @public
+//  */
+// module.exports = server;
+
+////////////////////////////////////////////
+
+/**
+ * Create HTTP server.
+ */
 
 // listen to requests
-server.listen(port, () => logger.info(`server started on port ${port} (${env})`));
+app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
 
 /**
  * Exports express
  * @public
  */
-module.exports = server;
+module.exports = app;
