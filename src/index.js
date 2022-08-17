2bc7f0bcdf8eec9c0fab6044ce1aae02cd83b7ed
CryptoSnowPrince <topdirector2017@gmail.com>
2022-08-17 08:51:27 +0200

    switch on http server

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
 // make bluebird default Promise
 const { port, env } = require("./config/vars");
-const fs = require('fs')
-const https = require('https')
+// const fs = require('fs')
+// const https = require('https')
 const logger = require("./config/logger");
 const app = require("./config/express");
 
-/**
- * Create HTTPS server.
- */
+// /**
+//  * Create HTTPS server.
+//  */
 
- const server = https.createServer({
-    key: fs.readFileSync('/etc/letsencrypt/live/cryptosnowprince.com/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/cryptosnowprince.com/fullchain.pem'),
-}, app)
-
-// listen to requests
-server.listen(port, () => logger.info(`server started on port ${port} (${env})`));
-
-/**
- * Exports express
- * @public
- */
-module.exports = server;
-
-/**
- * Create HTTP server.
- */
+//  const server = https.createServer({
+//     key: fs.readFileSync('/etc/letsencrypt/live/cryptosnowprince.com/privkey.pem'),
+//     cert: fs.readFileSync('/etc/letsencrypt/live/cryptosnowprince.com/fullchain.pem'),
+// }, app)
 
 // // listen to requests
-// app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
+// server.listen(port, () => logger.info(`server started on port ${port} (${env})`));
 
 // /**
 //  * Exports express
 //  * @public
 //  */
-// module.exports = app;
+// module.exports = server;
+
+/**
+ * Create HTTP server.
+ */
+
+// listen to requests
+app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
+
+/**
+ * Exports express
+ * @public
+ */
+module.exports = app;
